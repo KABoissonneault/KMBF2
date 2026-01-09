cfeb90d7b028f52fb60543d41f7769cb99126797
kaboissonneault <kaboissonneault@gmail.com>
2026-01-09 09:59:08 -0500

    Fixed Lord Beyond the Grave's at-will Command Undead to have more than 1 round of Duration, by making the Caster level match the caster's character level instead of being hardcoded to 0

--- KMBF2/Blueprint/LichFixes.cs
+++ KMBF2/Blueprint/LichFixes.cs
@@ -5,8 +5,11 @@
 // http://www.boost.org/LICENSE_1_0.txt)
 
 using BlueprintCore.Blueprints.Configurators;
+using BlueprintCore.Blueprints.CustomConfigurators.UnitLogic.Abilities;
 using BlueprintCore.Blueprints.References;
+using Kingmaker.Armies.Components;
 using Kingmaker.Blueprints;
+using Kingmaker.UnitLogic.Mechanics.Components;
 
 namespace KMBF2.Blueprint
 {
@@ -29,6 +32,17 @@ namespace KMBF2.Blueprint
                 AddColdImmunity(UnitRefs.MythicLichSkeletonDualWielderUnit);
                 AddColdImmunity(UnitRefs.MythicLichSkeletonTankUnit);
                 AddColdImmunity(UnitRefs.MythicLichSkeletonTwoHandedUnit);
+            }
+
+            if(PatchUtils.StartPatch("Lord Beyond the Grave Command Undead Duration"))
+            {
+                AbilityConfigurator.For(AbilityRefs.LichCommandUndead)
+                    .EditComponent<ContextSetAbilityParams>(c =>
+                    {
+                        // -1 falls back to spellbook
+                        c.CasterLevel = -1;
+                    })
+                    .Configure();
             }
         }
     }
